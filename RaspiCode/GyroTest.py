f82813573432ab81e7c4cd3449d7f0f889713073
Kurama <renderosalexis17@gmail.com>
2018-04-14 11:09:18 -0500

    Wrote some code to test the Pi's ability to interface with the MPU-6050 Gyro/Accel combo. Untested. -Alexis

---
+++ RaspiCode/GyroTest.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+import smbus
+import math
+ 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+ 
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+ 
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) for rev 1
+address = 0x68       # via i2cdetect
+ 
+bus.write_byte_data(address, power_mgmt_1, 0)
+ 
+print "Gyro"
+print "--------"
+ 
+xout = read_word_2c(0x43)
+yout = read_word_2c(0x45)
+zout = read_word_2c(0x47)
+ 
+print "gyro_xout: ", ("%5d" % xout), " scaled: ", (xout / 131)
+print "gyro_yout: ", ("%5d" % yout), " scaled: ", (yout / 131)
+print "gyro_zout: ", ("%5d" % zout), " scaled: ", (zout / 131)
+ 
+print
+print "Accelerometer"
+print "---------------------"
+ 
+accel_xout = read_word_2c(0x3b)
+accel_yout = read_word_2c(0x3d)
+accel_zout = read_word_2c(0x3f)
+ 
+accel_xout_scaled = accel_xout / 16384.0
+accel_yout_scaled = accel_yout / 16384.0
+accel_zout_scaled = accel_zout / 16384.0
+ 
+print "accel_xout: ", ("%6d" % accel_xout), " scaled: ", accel_xout_scaled
+print "accel_yout: ", ("%6d" % accel_yout), " scaled: ", accel_yout_scaled
+print "accel_zout: ", ("%6d" % accel_zout), " scaled: ", accel_zout_scaled
+ 
+print "X Rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+print "Y Rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
